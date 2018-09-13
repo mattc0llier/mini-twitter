ae29c2dd9bf6f2c43d4cc0139a1e56bc919af08d
mattc0llier <matt.collier@hotmail.com>
2018-09-13 15:24:23 +0100

    ready to tranform twitter handles

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ const formNode = document.querySelector("form");
 
 const timelineNode = document.querySelector(".timeline");
 
+let currentCharCount = 0;
+
 const charCount = function(input) {
   const pNode = document.querySelector(".counter");
   pNode.innerHTML = `Character count: ${input}`;
   pNode.style.color = input < 25 ? "black" : "red";
+  currentCharCount = input;
 };
 
 const submitTweet = function() {
@@ -18,11 +21,14 @@ const submitTweet = function() {
   // create button node
   const button = document.createElement("button");
   button.textContent = "x";
+  // appending delete button to tweet node
   tweetNode.appendChild(button);
+  //append tweet node to timelineNode
   timelineNode.appendChild(tweetNode);
-  input.value = "";
+  // clears text area
+  // init character count
   charCount("0");
-
+  //Setting event listener on delete button
   button.addEventListener("click", function(event) {
     timelineNode.removeChild(event.target.parentElement);
   });
@@ -30,10 +36,30 @@ const submitTweet = function() {
 
 formNode.addEventListener("submit", function(event) {
   event.preventDefault();
-  submitTweet();
+  const inputText = document.querySelector(".text-area-input");
+  if (currentCharCount > 0) {
+    // return tranformed mapped array here
+    transformTwitterHandle(inputText.value);
+    submitTweet();
+    // console.log(inputText.value);
+  }
+  inputText.value = "";
 });
 
 formNode.addEventListener("keyup", function(event) {
   const inputText = document.querySelector(".text-area-input");
   charCount(inputText.textLength);
 });
+
+const transformTwitterHandle = function(string) {
+  //console.log(string.split(" "));
+  let wordArray = string.split(" ");
+
+  const transformedWordsArr = wordArray.map(function(word) {
+    // console.log(word);
+    const atRegex = /@/g;
+    if (word.match(atRegex) && word[0] === "@") {
+      console.log("@");
+    }
+  });
+};
